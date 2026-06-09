1567c7289fff9a6e8afcee0da0985fd3a39b7e86
Ar1maa-afk <moyagabriel17@gmail.com>
2026-06-08 19:22:06 -0600

    Tarea 100% terminada

--- Tarea1Unidad1/Bloque5/JuegodeGato.cs
+++ Tarea1Unidad1/Bloque5/JuegodeGato.cs
@@ -1,12 +1,95 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Tarea1Unidad1.Bloque5
 {
     public class JuegodeGato
     {
-        
+        public JuegodeGato()
+        {
+            bool reiniciar = true;
+
+            while (reiniciar)
+            {
+                char[,] tablero =
+                {
+                    { '-', '-', '-' },
+                    { '-', '-', '-' },
+                    { '-', '-', '-' }
+                };
+
+                char jugador = 'X';
+                bool ganador = false;
+                int movimientos = 0;
+
+                while (!ganador && movimientos < 9)
+                {
+                    Console.Clear();
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            Console.Write(tablero[i, j] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine("Jugador " + jugador);
+
+                    Console.Write("Fila (0-2): ");
+                    int.TryParse(Console.ReadLine(), out int fila);
+
+                    Console.Write("Columna (0-2): ");
+                    int.TryParse(Console.ReadLine(), out int columna);
+
+                    if (fila >= 0 && fila < 3 &&
+                        columna >= 0 && columna < 3 &&
+                        tablero[fila, columna] == '-')
+                    {
+                        tablero[fila, columna] = jugador;
+                        movimientos++;
+
+                        for (int i = 0; i < 3; i++)
+                        {
+                            if (tablero[i,0] == jugador &&
+                                tablero[i,1] == jugador &&
+                                tablero[i,2] == jugador)
+                                ganador = true;
+
+                            if (tablero[0,i] == jugador &&
+                                tablero[1,i] == jugador &&
+                                tablero[2,i] == jugador)
+                                ganador = true;
+                        }
+
+                        if ((tablero[0,0] == jugador &&
+                             tablero[1,1] == jugador &&
+                             tablero[2,2] == jugador) ||
+
+                            (tablero[0,2] == jugador &&
+                             tablero[1,1] == jugador &&
+                             tablero[2,0] == jugador))
+                        {
+                            ganador = true;
+                        }
+
+                        if (!ganador)
+                        {
+                            jugador = jugador == 'X' ? 'O' : 'X';
+                        }
+                    }
+                }
+
+                if (ganador)
+                {
+                    Console.WriteLine("Ganó el jugador " + jugador);
+                }
+                else
+                {
+                    Console.WriteLine("Empate");
+                }
+
+                Console.WriteLine("¿Reiniciar? 1=Sí 0=No");
+                reiniciar = Console.ReadLine() == "1";
+            }
+        }
     }
 }
--- Tarea1Unidad1/Bloque5/MatrizdeNotasporParcial.cs
+++ Tarea1Unidad1/Bloque5/MatrizdeNotasporParcial.cs
@@ -1,12 +1,74 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Tarea1Unidad1.Bloque5
 {
     public class MatrizdeNotasporParcial
     {
-        
+        public MatrizdeNotasporParcial(int estudiantes)
+        {
+            double[,] notas = new double[estudiantes, 3];
+
+            for (int i = 0; i < estudiantes; i++)
+            {
+                Console.WriteLine("Estudiante " + (i + 1));
+
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("Parcial " + (j + 1) + ": ");
+                    double.TryParse(Console.ReadLine(), out notas[i, j]);
+                }
+            }
+
+            double mejorPromedio = 0;
+            int mejorEstudiante = 0;
+
+            Console.WriteLine("\nPromedio por estudiante:");
+
+            for (int i = 0; i < estudiantes; i++)
+            {
+                double suma = 0;
+
+                for (int j = 0; j < 3; j++)
+                {
+                    suma += notas[i, j];
+                }
+
+                double promedio = suma / 3;
+
+                Console.WriteLine("Estudiante " + (i + 1) + ": " + promedio);
+
+                if (i == 0 || promedio > mejorPromedio)
+                {
+                    mejorPromedio = promedio;
+                    mejorEstudiante = i + 1;
+                }
+            }
+
+            double promedioMasBajo = 0;
+            int parcialMasDificil = 0;
+
+            Console.WriteLine("\nPromedio por parcial:");
+
+            for (int j = 0; j < 3; j++)
+            {
+                double suma = 0;
+
+                for (int i = 0; i < estudiantes; i++)
+                {
+                    suma += notas[i, j];
+                }
+
+                double promedio = suma / estudiantes;
+
+                Console.WriteLine("Parcial " + (j + 1) + ": " + promedio);
+
+                if (j == 0 || promedio < promedioMasBajo)
+                {
+                    promedioMasBajo = promedio;
+                    parcialMasDificil = j + 1;
+                }
+            }
+
+            Console.WriteLine("\nMejor estudiante: " + mejorEstudiante);
+            Console.WriteLine("Parcial más difícil: " + parcialMasDificil);
+        }
     }
 }
